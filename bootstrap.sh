5958b9636b64ad3a9e1b4364dccff40062a3f658
Scott Haleen <scott.haleen@gmail.com>
2014-10-08 14:10:22 -0400

    added topic clustering patch

--- bootstrap.sh
+++ bootstrap.sh
@@ -56,6 +56,9 @@ cd /srv/software
 
 git clone https://github.com/mitll/topic-clustering
 
+# patch
+sudo cp /vagrant/artifacts/run_all.py /srv/software/topic-clustering/topic/
+
 
 printf "Creating \e[0;32mActiveSearch\e[0m \n"
 
