7e08d77f9a1421344d66c62af66ba79bd50db8a5
Scott Haleen <scott.haleen@gmail.com>
2014-10-28 08:58:58 -0400

    updated url

--- bootstrap.sh
+++ bootstrap.sh
@@ -110,12 +110,12 @@ sudo chmod 777 /srv/software/tika
 
 cd /srv/software/tika
 
-wget http://www.apache.org/dyn/closer.cgi/tika/tika-app-1.6.jar
+wget http://apache.mirrors.lucidnetworks.net/tika/tika-app-1.6.jar
+
 printf "99df0d8c3f6a2be498d275053e611fb5afdf0a9d  tika-app-1.6.jar" | sha1sum -c -
 
 if [[ $? -ne 0 ]]; then
     printf "\e[0;31mApache Tika check sum failed\e[0m \n"
-    sudo rm -rf /srv/software/tika/*
 fi
 
 cd /srv/software 
