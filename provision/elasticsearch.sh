a466780e445732136f74dba50894fb12772c4a37
Scott Haleen <scott.haleen@gmail.com>
2015-07-20 15:22:10 -0400

    updated elasticsearch version

--- provision/elasticsearch.sh
+++ provision/elasticsearch.sh
@@ -4,8 +4,8 @@ printf "Installing \e[0;36mElastic Search\e[0m \n"
 
 cd /srv/software/
 
-sudo wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.3.4.deb
-sudo dpkg -i /srv/software/elasticsearch-1.3.4.deb
+sudo wget https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.7.0.deb
+sudo dpkg -i /srv/software/elasticsearch-1.7.0.deb
 
 sudo update-rc.d elasticsearch defaults 95 10
 sudo /etc/init.d/elasticsearch start
