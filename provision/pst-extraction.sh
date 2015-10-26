d9ff3b586871dfaeac945b32b680d2e24194b15c
Elliot Ickovic <elliot.ickovic@gmail.com>
2015-10-26 16:24:35 -0400

    script to copy mitie for pst extraction

--- provision/pst-extraction.sh
+++ provision/pst-extraction.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-
 printf "Installing \e[0;36mpst-extraction\e[0m \n"
 
-sudo cd /srv/software 
+cd /srv/software
 
 sudo wget -O - -o /dev/null https://github.com/Sotera/pst-extraction/archive/master.tar.gz | tar -zxvf -
+
+/srv/software/pst-extraction-master/
+cp /srv/software/MITIE/MITIE-models/english/ner_model.dat /srv/software/pst-extraction-master/
+cp /srv/software/MITIE/mitielib/libmitie.* /srv/software/pst-extraction-master/
+cp /srv/software/MITIE/mitielib/mitie.py /srv/software/pst-extraction-master/
+
