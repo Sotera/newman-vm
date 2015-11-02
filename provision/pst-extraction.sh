add3d95538465ea42695346fc3cd5ee1ef3917fb
Elliot Ickovic <elliot.ickovic@gmail.com>
2015-11-02 15:00:21 -0500

    updated installer to download geo/ ip indexing data

--- provision/pst-extraction.sh
+++ provision/pst-extraction.sh
@@ -6,8 +6,15 @@ cd /srv/software
 
 sudo wget -O - -o /dev/null https://github.com/Sotera/pst-extraction/archive/master.tar.gz | tar -zxvf -
 
-/srv/software/pst-extraction-master/
 cp /srv/software/MITIE/MITIE-models/english/ner_model.dat /srv/software/pst-extraction-master/
 cp /srv/software/MITIE/mitielib/libmitie.* /srv/software/pst-extraction-master/
 cp /srv/software/MITIE/mitielib/mitie.py /srv/software/pst-extraction-master/
 
+
+cd /srv/software/pst-extraction-master/
+
+#setup location index
+sudo ./bin/build_clavin_index.sh
+
+#setup geoip index 
+sudo ./bin/setup_geo2ip.sh
