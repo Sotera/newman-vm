785c41fe9763ac1362e7e541616d8ac6e009f3c3
Elliot Ickovic <elliot.ickovic@gmail.com>
2016-01-27 19:53:01 -0500

    Configure docker service

---
+++ provision/docker.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo cp /vargrant/artifacts/docker.list /etc/apt/sources.list.d/docker.list
+
+sudo apt-get update
+sudo apt-get purge lxc-docker
+
+#install prereqs
+sudo apt-get install linux-image-extra-$(uname -r)
+
+sudo apt-get install docker-engine
+sudo service docker start
+
+
+#add docker group
+sudo usermod -aG docker ubuntu
+
+
