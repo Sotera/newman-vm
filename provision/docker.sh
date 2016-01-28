b86d1141537a4e62d241775308cef1ed8d3d43c4
Elliot Ickovic <elliot.ickovic@gmail.com>
2016-01-27 23:23:06 -0500

    Fixed type in script.  Added exif reader lib.

--- provision/docker.sh
+++ provision/docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo cp /vargrant/artifacts/docker.list /etc/apt/sources.list.d/docker.list
+sudo cp /vagrant/artifacts/docker.list /etc/apt/sources.list.d/docker.list
 
 sudo apt-get update
 sudo apt-get purge lxc-docker
