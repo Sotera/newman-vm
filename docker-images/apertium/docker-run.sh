9e08c97723331e185ea991442ec2ae3e4c879062
Elliot Ickovic <elliot.ickovic@gmail.com>
2016-03-02 14:19:40 -0500

    Patched broken script and added comments

--- docker-images/apertium/docker-run.sh
+++ docker-images/apertium/docker-run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
-# run from anywhere but need to pass in script name
-# e.g. 
-#docker run -ti --rm -P -v /QCR/pst-extraction-master-temp/:/srv/software/pst-extraction/ apertium ./bin/run_spark_translate.sh
+# Run a local spark install but need to pass in script name as follows -- otherwise will just return as spark-submit help
+# e.g. this will run the apertium translation 
+#docker run -ti --rm -P -v /QCR/pst-extraction-master-temp/:/srv/software/pst-extraction/ apertium ./bin/run_spark_translation.sh
 
 docker run -ti --rm -P -v /QCR/pst-extraction-master-temp/:/srv/software/pst-extraction/ apertium
