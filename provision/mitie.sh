a0b6ca4aad717386eb11b9068b08bda70c6f8565
John Reeme <john.reeme@soteradefense.com>
2016-07-12 10:28:36 -0500

    Did a few more scripts. Now need to come back around and adress build user/location issues

--- provision/mitie.sh
+++ provision/mitie.sh
@@ -1,46 +1,49 @@
 #!/usr/bin/env bash
 
+su vagrant <<< "vagrant"
 
-apt-get -y install cmake
+sudo apt-get -y install cmake
 #apt-get -y install libopenblas-base libopenblas-dev
 
 printf "Creating \e[0;36mMITIE\e[0m \n"
 
-cd /srv/software 
-
+pushd /srv/software/
 git clone https://github.com/mit-nlp/MITIE
+popd
 
-cd /srv/software/MITIE
-
-if [ -a /vagrant/artifacts/MITIE-models-v0.2.tar.bz2 ]; then
-    cp /vagrant/artifacts/MITIE-models-v0.2.tar.bz2 /srv/software/MITIE/
+if [ -a ../artifacts/MITIE-models-v0.2.tar.bz2 ]; then
+    cp ../artifacts/MITIE-models-v0.2.tar.bz2 /srv/software/MITIE/
+    pushd /srv/software/MITIE/
     tar -xjf MITIE-models-v0.2.tar.bz2
+    popd
 else
-    make MITIE-models
+    pushd /srv/software/MITIE/
+    sudo make MITIE-models
+    popd
 fi 
 
-if [ -a /vagrant/artifacts/MITIE-models-v0.2-Spanish.zip ]; then
-    cp /vagrant/artifacts/MITIE-models-v0.2-Spanish.zip /srv/software/MITIE/
+if [ -a ../artifacts/MITIE-models-v0.2-Spanish.zip ]; then
+    cp ../artifacts/MITIE-models-v0.2-Spanish.zip /srv/software/MITIE/
+    pushd /srv/software/MITIE/
     unzip MITIE-models-v0.2-Spanish.zip
+    popd
 else
+    pushd /srv/software/MITIE/
     sudo wget https://sourceforge.net/projects/mitie/files/binaries/MITIE-models-v0.2-Spanish.zip    
     unzip MITIE-models-v0.2-Spanish.zip
+    popd
 fi 
 
-cd tools/ner_stream
+mkdir -p /srv/software/MITIE/tools/ner_stream/build
+pushd /srv/software/MITIE/tools/ner_stream/build
+sudo cmake ..
+sudo cmake --build . --config Release
+popd
 
-mkdir build
-
-cd build
-
-cmake ..
-
-cmake --build . --config Release
-
-cd /srv/software/MITIE/mitielib
-make
-
+pushd /srv/software/MITIE/mitielib
+sudo make
 mkdir build
 cd build
-cmake ..
-cmake --build . --config Release --target install
+sudo cmake ..
+sudo cmake --build . --config Release --target install
+popd
