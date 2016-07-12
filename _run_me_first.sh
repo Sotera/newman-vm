148bf82062194dc4641fed2873127d7a6d44f6be
nobody <nobody@nowhere.com>
2016-07-12 12:56:27 -0500

    Hacking away!

--- _run_me_first.sh
+++ _run_me_first.sh
@@ -5,7 +5,6 @@ sudo useradd -ms /bin/bash vagrant && sudo adduser vagrant sudo
 printf "Enter new password for user \e[0;36m'vagrant'\e[0m \n"
 sudo passwd vagrant
 printf "One more time!\n"
-su vagrant
-cd ~
-git clone https://github.com/sotera/newman-vm
-cd ~/newman-vm
+cd ..
+sudo -u vagrant -H sh -c "sudo mv newman-vm/ /home/vagrant"
+printf "Now, logout and log back in as 'vagrant' then cd /home/vagrant/newman-vm && ./_run_me_second.sh"
