9d9081794ce4cfb37f9f42d1310d1418d123a02a
nobody <nobody@nowhere.com>
2016-07-14 14:33:19 -0500

    Take 7

--- _run_me_first.sh
+++ _run_me_first.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+#Try doing this java-repo thing here ...
+pushd provision
+. java-repo.sh
+popd
+
 printf "Attempting to create user & group \e[0;36m'vagrant'\e[0m\n"
 sudo useradd -ms /bin/bash vagrant && sudo adduser vagrant sudo
 printf "Enter new password for user \e[0;36m'vagrant'\e[0m\n"
@@ -8,10 +13,5 @@ printf "One more time!\n"
 cd ..
 sudo -u vagrant -H sh -c "sudo mv newman-vm/ /home/vagrant"
 sudo chown -R vagrant.vagrant /home/vagrant/newman-vm
-
-#Try doing this java-repo thing here ...
-pushd provision
-. java-repo.sh
-popd
 
 printf "Now, logout and log back in as 'vagrant' then cd /home/vagrant/newman-vm && ./_run_me_second.sh\n"
--- provision/java.sh
+++ provision/java.sh
@@ -6,4 +6,5 @@ sudo apt-get -y install oracle-java8-installer
 printf "Install \e[0;36mMaven\e[0m \n"
 sudo apt-get -y install maven
  
+. export JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:bin/javac::")
 echo 'export JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:bin/javac::")' >> ~/.bashrc
