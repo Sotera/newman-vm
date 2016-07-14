0150bf48c8d141c3c612643eaeaf4ab08b21ab2b
abc <a@b.c>
2016-07-14 12:24:55 -0500

    Change JAVA_HOME algorithm

--- provision/java.sh
+++ provision/java.sh
@@ -6,4 +6,4 @@ sudo apt-get -y install oracle-java8-installer
 printf "Install \e[0;36mMaven\e[0m \n"
 sudo apt-get -y install maven
  
-echo 'export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/javac::")' >> ~/.bashrc
+echo 'export JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:bin/javac::")' >> ~/.bashrc
