278ffec012982b44ace5fe7e4fc41b63121b5544
John Reeme <john.reeme@soteradefense.com>
2016-07-20 15:42:41 -0500

    Patch to get newman-research branch on-the-metal

--- provision/newman-research.sh
+++ provision/newman-research.sh
@@ -5,8 +5,8 @@ printf "Installing \e[0;36mnewman-research\e[0m \n"
 #install the newman-research project
 pushd /srv/software/
 
-sudo wget -O - -o /dev/null https://github.com/Sotera/newman-research/archive/master.tar.gz | tar -zxvf -
-
+sudo wget -O - -o /dev/null https://github.com/Sotera/newman-research/archive/on-the-metal.tar.gz | tar -zxvf -
+sudo mv newman-research-on-the-metal newman-research-master
 popd
 
 pushd /srv/software/newman-research-master/docker_opencv_spark_ocr
