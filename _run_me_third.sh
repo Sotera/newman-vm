e87ccdba2d30ac6eab965910a37ac0e000ab5f38
John Reeme <john.reeme@soteradefense.com>
2016-07-21 10:55:40 -0500

    Add pulling of Docker containers

--- _run_me_third.sh
+++ _run_me_third.sh
@@ -10,6 +10,16 @@ pushd provision
 
 popd
 
+printf "Pulling the Docker extraction support containers ...\n"
+docker pull sotera/spark-local:0.9
+docker tag sotera/spark-local:0.9 spark-local:latest
+docker pull sotera/geo-utils:0.9
+docker tag sotera/geo-utils:0.9 geo-utils:latest
+docker pull sotera/apertium:0.9
+docker tag sotera/apertium:0.9 apertium:latest
+docker pull sotera/ocr:0.9
+docker tag sotera/ocr:0.9 ocr:latest
+
 printf "Fix permissions on /srv/software\n"
 sudo chown -R vagrant.vagrant /srv/software
 
