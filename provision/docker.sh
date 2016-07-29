dc3d0af54087d8c9adfd2e92475fa20be9707558
John Reeme <john.reeme@soteradefense.com>
2016-07-29 17:32:27 +0000

    Tweak docker sources

--- provision/docker.sh
+++ provision/docker.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates
+
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo cp /vagrant/artifacts/docker.list /etc/apt/sources.list.d/docker.list
+sudo cp /home/vagrant/newman-vm/artifacts/docker.list /etc/apt/sources.list.d/docker.list
 
 sudo apt-get update
 sudo apt-get purge lxc-docker
