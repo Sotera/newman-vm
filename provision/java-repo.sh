bc74233de4a54dc3be5f3fbdff44f445b4f9708b
Elliot Ickovic <elliot.ickovic@gmail.com>
2016-08-08 13:10:10 -0400

    Fix broken java8 installer Fix path issue with non vagrant usage

--- provision/java-repo.sh
+++ provision/java-repo.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
-
 printf "Add  \e[0;36mJava Repo\e[0m \n"
-add-apt-repository -y ppa:webupd8team/java
 
-echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
-echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee /etc/apt/sources.list.d/webupd8team-java.list
+echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
+apt-get update
+apt-get install oracle-java8-installer
+
+#accept oracle license
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+
+#set env
+sudo apt-get install oracle-java8-set-default
 
