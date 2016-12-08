1ca8314e50f89a2b90532de5d602a9c916ac2831
eickovic <elliot.ickovic@gmail.com>
2016-12-08 11:29:14 -0500

    Fix a few problems with nginx deployment scripts.

--- provision/nginx_login_ssl_proxy.sh
+++ provision/nginx_login_ssl_proxy.sh
@@ -1,19 +1,28 @@
 sudo apt-get update
-sudo apt-get install nginx
+sudo apt-get -y install nginx
 
-
-cd /etc/nginx
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/cert.key -out /etc/nginx/cert.crt
 
-sudo cp artifacts/newman--https_rproxy /etc/nginx/sites-available/newman
-sudo rm /etc/gninx/sites-enabled/default
-sudo ln -s /etc/nginx/sites-available/newman /etc/nginx/sites-enable/newman
+sudo cp artifacts/newman--ssl_ba_rproxy /etc/nginx/sites-available/newman
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -s /etc/nginx/sites-available/newman /etc/nginx/sites-enabled/newman
 
 sudo mkdir -p /var/www/newman/
 sudo chown -R vagrant:vagrant /var/www/newman
 
 
-#
+
+RED='\033[0;31m'
+NC='\033[0m'
+
+echo "========================================================================================"
+echo -e "${RED}Add users to .htpasswd file using the following two commands:${NC}"
+echo "sudo sh -c \"echo -n 'vagrant:' >> /etc/nginx/.htpasswd\""
+echo "sudo sh -c \"openssl passwd -apr1 >> /etc/nginx/.htpasswd\""
+echo "...repeat for each user"
+echo "========================================================================================"
+
+
 #  Add users to .htpasswd will prompt for passwd
 #  e.g. vagrant users / pass
 #  sudo sh -c "echo -n 'vagrant:' >> /etc/nginx/.htpasswd"
